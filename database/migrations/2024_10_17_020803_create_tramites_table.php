655846c0900864a4e53459d5987555ef646a2083
Roberto Carlos Ramirez <78807484+RolosRCR@users.noreply.github.com>
2024-10-29 03:09:55 -0600

    Sesion de correcciones 2, Correcciones en el seeder

--- database/migrations/2024_10_17_020803_create_tramites_table.php
+++ database/migrations/2024_10_17_020803_create_tramites_table.php
@@ -10,16 +10,24 @@ class CreateTramitesTable extends Migration
     {
         Schema::create('tramites', function (Blueprint $table) {
             $table->increments('id_tramite');
-            $table->unsignedInteger('id_libro'); // Referencia manual a la tabla libros
-            $table->unsignedInteger('id_beneficiario'); // Referencia manual a la tabla beneficiarios
+            $table->unsignedInteger('id_libro');
+            $table->unsignedInteger('id_beneficiario');
             $table->date('fecha')->default(now());
             $table->integer('tipo'); // 1: Préstamo, 2: Entrega
+
+            // Agregar claves foráneas
+            $table->foreign('id_libro')->references('id_libro')->on('libros')->onDelete('cascade');
+            $table->foreign('id_beneficiario')->references('id_beneficiario')->on('beneficiarios')->onDelete('cascade');
         });
     }
 
     public function down()
     {
+        Schema::table('tramites', function (Blueprint $table) {
+            // Eliminar claves foráneas primero
+            $table->dropForeign(['id_libro']);
+            $table->dropForeign(['id_beneficiario']);
+        });
         Schema::dropIfExists('tramites');
     }
 }
-
